abf08ff64cebc677e3acbc24e79903c829f269fe
LeighAnne17 <leighndimande17@icloud.com>
2024-05-20 13:03:07 -0700

    airbnb clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,52 @@
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    __classes = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review
+    }
+
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        json_dict = {}
-        for key, obj in self.__objects.items():
-            json_dict[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as f:
-            json.dump(json_dict, f)
+        """Serializes __objects to the JSON file"""
+        serialized_objects = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(serialized_objects, f)
 
     def reload(self):
         try:
-            with open(self.__file_path, 'r') as f:
-                json_dict = json.load(f)
-                for key, val in json_dict.items():
+            with open(FileStorage.__file_path, 'r') as f:
+                data = json.load(f)
+                for key, val in data.items():
                     class_name, obj_id = key.split('.')
-                    cls = eval(class_name)
-                    obj = cls(**val)
-                    self.new(obj)
+                    if class_name == 'User':
+                        value['__class__'] = 'User'
+                        new_obj = User(**value)
+                    else:
+                        pass
+                    self.new(new_obj)
         except FileNotFoundError:
             pass
